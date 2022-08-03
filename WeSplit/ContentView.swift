cf083ebffa52f3cf4eb5957cf04a23e7da4391fb
Barry Barron <barrybbarron@gmail.com>
2022-08-03 16:01:51 -0700

    Project 3, Challenge 2 - WeSplit make total amount red when tip is zero

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Barry Barron on 7/14/22.
 //  Modified 7/23/22 to add challenge portions and other feature changes
-//  version 1.2 Final version all chanllenges inculded
+//  version 1.2 Final version all chanllenges included
 
 import SwiftUI
 
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var numberOfPeople = 0 // Changed default to 0 for 2 people
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
+    //@State private var useRedText = true ////
     
     let tipPercentages = [10, 15, 18, 20, 25, 0]  // Added 18%
     
@@ -64,21 +65,10 @@ struct ContentView: View {
                     Text("Tip percentage?")  // Changed from "How much tip do you want to leave?"
                 }
                 
-                
-//                Section { //this section removed - part of the 3rd challenge question
-//                    Picker("Tip percentage", selection: $tipPercentage) {
-//                        ForEach(2..<101) {
-//                            Text($0, format: .percent)
-//                        }
-//                    }
-//                    .pickerStyle(.menu)
-//                } header: {
-//                    Text("Tip percentage?")  // Changed from "How much tip do you want to leave?"
-//                }
-                
     
                 Section { // Challenge section added to show total receipt plus tip
                     Text(checkTotal, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary) // red when 0 tip
                 } header: {
                     Text("Receipt total with tip")  // Added header
                 }
